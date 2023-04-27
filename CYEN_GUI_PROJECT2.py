5822412ce729f3558ddc494c34fc5ecc5c2e0f2f
xavierl08 <131475965+xavierl08@users.noreply.github.com>
2023-04-27 15:26:22 -0500

    Update CYEN_GUI_PROJECT2.py

--- CYEN_GUI_PROJECT2.py
+++ CYEN_GUI_PROJECT2.py
@@ -1,24 +1,33 @@
 from tkinter import *
+import text
+import RPi.GPIO as GPIO
+from time import sleep
+from random import randint
 
-buttonDisplays = [
-    ["LUDia","Up","RUDia"],
-    ["Left","Center","Right"],
-    ["LDDia","Down","RDDia"]
-]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.IN)
+GPIO.setup(12,GPIO.IN)
+GPIO.setup(27,GPIO.IN)
+GPIO.setup(23,GPIO.IN)
+GPIO.setup(21,GPIO.IN)
+GPIO.setup(25,GPIO.IN)
+#17,12,27,23,21,25
+# outputs LED and possibly speaker
+path1= [17,12]
+end1=27
+path2=[21,25]
+end2=23
 
-folder = "CYEN_images/"
-buttonDef = {
-    "info": "[]",
-    "LUDia": "^<-",
-    "Up": "^^",
-    "RUDia": "->^",
-    "Left": "<-",
-    "Center": "0",
-    "Right": "->",
-    "LDDia": "d<-",
-    "Down": "D",
-    "RDDia": "->d"
-}
+paths=[path1,path2]
+correctpath = paths[randint(0,1)]
+if 17 in correctpath:
+    correctend = end1
+else:
+    correctend = end2
+
+ballmotion = []
+if GPIO.input(17):
+    ballmotion.append(17)
 window = Tk()
 
 def switch():
@@ -29,33 +38,24 @@ def switchBack():
     f1.pack()
     f2.forget()
 
-def process():
-    pass
+
 
 f1 = Frame(window)
 f2 = Frame(window)
-l1 = Label(f1, text="MARBLE MAZE", font=("TexGyreAdventure",20))
+l1 = Label(f1, text="MARBLE MAZE", font=("Courier",30))
 l1.grid(row=0,column=1,sticky=N+S+E+W)
-infoB = Button(f1,text="info",font=("TexGyreAdventure",10),command=switch)
+infoB = Button(f1,text="Info",font=("Courier",10),command=switch)
 infoB.grid(row=1,column=1,sticky=N+S+E+W)
-backB = Button(f2,text="back",font=("TexGyreAdventure",10),command=switchBack)
+backB = Button(f2,text="Back",font=("Courier",10),command=switchBack)
 backB.grid(row=0,column=1,sticky=N+S+E+W)
+t1 = text.text_window(f2)
 
-for row in range(len(buttonDisplays)):
-    for column in range(len(buttonDisplays[row])):
-        img = PhotoImage(file=folder+buttonDisplays[row][column]+".png")
-        button = Button(f1, bg="white", image=img, borderwidth=0, highlightthickness=0,activebackground="white", command=process)
-        button.image = img
-        button.grid(row=row+2, column=column, sticky=N+S+E+W)
-# LUimg = PhotoImage(file=folder+"LUDia.gif")
-# LU = Button(f1,image=LUimg,borderwidth=0,highlightthickness=0,activebackground="white",command=process)
-# LU.grid
-# LU.image = LUimg
 
 
 
 window.title("CYEN Marble Maze 1.0")
 f1.pack()
 window.mainloop()
+
 
 
